28c43340acd6fa0e2b78d5afa3d9cb4a2a2a0b90
sinji <tobeabestns@naver.com>
2024-10-30 14:05:07 +0900

    refactor(PlayerMovingLogic): position 값 지정 방식 변경('position -= 1' -> 'position--')

--- src/main/java/domain/PlayerMovingLogic.java
+++ src/main/java/domain/PlayerMovingLogic.java
@@ -9,12 +9,10 @@ public class PlayerMovingLogic {
         this.ladderCollection = ladderCollection;
     }
 
-    // 모든 사다리 층을 순회하며 플레이어 이동 처리
     public void updatePlayerPositions(List<Player> players) {
         for (Player player : players) {
             movePlayers(player);
         }
-
     }
 
     private void movePlayers(Player player) {
@@ -23,11 +21,11 @@ public class PlayerMovingLogic {
         for (List<String> ladderRow : ladderCollection.getLadderLayers()) {
             // 현재 위치에서 오른쪽에 연결 다리가 있는 경우
             if (position * 2 + 1 < ladderRow.size() && ladderRow.get(position * 2 + 1).equals("-----")) {
-                position += 1;
+                position++;
             }
             // 현재 위치에서 왼쪽에 연결 다리가 있는 경우
             else if (position * 2 - 1 >= 0 && ladderRow.get(position * 2 - 1).equals("-----")) {
-                position -= 1;
+                position--;
             }
             player.setPosition(position);
         }
