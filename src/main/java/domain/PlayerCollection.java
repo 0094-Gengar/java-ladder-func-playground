c60e16c73bef45afe4dfccd8b31d772864949bac
sinji <tobeabestns@naver.com>
2024-11-20 19:16:03 +0900

    refactor(PlayerCollection): 리팩토링(split 함수 사용)

--- src/main/java/domain/PlayerCollection.java
+++ src/main/java/domain/PlayerCollection.java
@@ -7,22 +7,9 @@ public class PlayerCollection {
 
     public static List<Player> createPlayerCollection(String input) {
         List<Player> players = new ArrayList<>();
-        StringBuilder nameBuilder = new StringBuilder();
         int position = 0;
-
-        for (int i = 0; i < input.length(); i++) {
-            char currentChar = input.charAt(i);
-
-            if (currentChar == ',') {
-                players.add(new Player(nameBuilder.toString().trim(), position++));
-                nameBuilder.setLength(0);
-            }
-            if (currentChar != ',') {
-                nameBuilder.append(currentChar);
-            }
-        }
-        if (!nameBuilder.isEmpty()) {
-            players.add(new Player(nameBuilder.toString().trim(), position++));
+        for (String name : input.split(",")) {
+            players.add(new Player(name.trim(), position++));
         }
         return players;
     }
