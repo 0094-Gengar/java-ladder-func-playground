353d5a9947967933f474e528bc55addb2fe9db08
sinji <tobeabestns@naver.com>
2024-11-20 17:22:40 +0900

    refactor(LadderController): 코드 리팩토링(클래스 필드 수 줄이기)

--- src/main/java/controller/LadderController.java
+++ src/main/java/controller/LadderController.java
@@ -6,24 +6,20 @@ import domain.PlayerCollection;
 import domain.PlayerMovingLogic;
 import view.InputView;
 import view.OutputView;
+
 import java.util.List;
 
 public class LadderController {
     private List<Player> players;
-    private String inputResults;
     private LadderCollection ladderCollection;
     private PlayerMovingLogic playerMovingLogic;
-    private OutputView outputView;
-    private static final int LADDERS_MIN_HEIGHT = 1;
 
     public void run() {
         inputPlayerNames();
-        inputPrizes();
         generateLadders();
         printResultOfLadders();
         startLadderGame();
         resultOfLadderGames();
-
         Runtime.getRuntime().addShutdownHook(new Thread(InputView::closeScanner));
     }
 
@@ -32,30 +28,26 @@ public class LadderController {
         this.players = PlayerCollection.createPlayerCollection(inputPlayers);
     }
 
-    private void inputPrizes() {
-        this.inputResults = InputView.inputPrizeOfResult();
-    }
-
     private void generateLadders() {
         int heightOfLadder = InputView.ladderHeight();
         validateLadderHeight(heightOfLadder);
-        // 예외 처리
-
         InputView.clearScannerBuffer();
+        String inputResults = InputView.inputPrizeOfResult();
         int widthOfLadder = players.size();
         this.ladderCollection = new LadderCollection(heightOfLadder, widthOfLadder);
         this.playerMovingLogic = new PlayerMovingLogic(ladderCollection);
-        this.outputView = new OutputView(ladderCollection, players, inputResults);
+        new OutputView(ladderCollection, players, inputResults);
     }
 
     public static void validateLadderHeight(final int heightOfLadder) {
+        final int LADDERS_MIN_HEIGHT = 1;
         if (heightOfLadder < LADDERS_MIN_HEIGHT) {
             throw new IllegalArgumentException("사다리의 높이는 1 이상이어야 합니다.");
         }
     }
 
     private void printResultOfLadders() {
-        outputView.resultOfLadders();
+        OutputView.resultOfLadders();
     }
 
     private void startLadderGame() {
@@ -64,6 +56,6 @@ public class LadderController {
 
     private void resultOfLadderGames() {
         String playerName = InputView.inputPlayerPrize();
-        outputView.resultOfPrize(playerName);
+        OutputView.resultOfPrize(playerName);
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -7,9 +7,9 @@ import domain.PrizeCollection;
 import java.util.List;
 
 public class OutputView {
-    private LadderCollection ladderCollection;
-    private List<Player> players;
-    private List<String> prizes;
+    private static LadderCollection ladderCollection;
+    private static List<Player> players;
+    private static List<String> prizes;
 
     public OutputView(LadderCollection ladderCollection, List<Player> players, String results) {
         this.ladderCollection = ladderCollection;
@@ -17,7 +17,7 @@ public class OutputView {
         this.prizes = PrizeCollection.createPrizeCollection(results);
     }
 
-    public void resultOfLadders() {
+    public static void resultOfLadders() {
         System.out.println("사다리 결과");
         printPlayerNames();
         ladderCollection.getLadderLayers().forEach(row ->
@@ -26,23 +26,22 @@ public class OutputView {
         printPrize();
     }
 
-    private void printPlayerNames() {
+    private static void printPlayerNames() {
         players.forEach(player ->
                 System.out.print(String.format("%-5s ", player.getName()))
         );
         System.out.println();
     }
 
-    private void printPrize() {
+    private static void printPrize() {
         prizes.forEach(prize ->
                 System.out.print(String.format("%-5s ", prize))
         );
         System.out.println();
     }
 
-    public void resultOfPrize(String playerName) {
+    public static void resultOfPrize(String playerName) {
         System.out.println("실행 결과");
-
         if ("all".equals(playerName)) {
             players.forEach(player ->
                     System.out.printf("%s : %s\n", player.getName(), getResultForPlayer(player.getName()))
@@ -53,7 +52,7 @@ public class OutputView {
         }
     }
 
-    private String getResultForPlayer(String playerName) {
+    private static String getResultForPlayer(String playerName) {
         for (Player player : players) {
             if (player.getName().equals(playerName)) {
                 int position = player.getPosition();
